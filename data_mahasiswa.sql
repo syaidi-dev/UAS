6849df0cfa90fe9c8220701c78dd677b7036daf0
syaidi-dev <75831400+syaidi-dev@users.noreply.github.com>
2021-02-02 12:51:24 +0800

    Add files via upload

---
+++ data_mahasiswa.sql
@@ -0,0 +1,35 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.24 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for db_mahasiswa
+CREATE DATABASE IF NOT EXISTS `db_mahasiswa` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `db_mahasiswa`;
+
+-- Dumping structure for table db_mahasiswa.tb_mahasiswa
+CREATE TABLE IF NOT EXISTS `tb_mahasiswa` (
+  `npm` varchar(8) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `tempat_lahir` varchar(50) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  `alamat` text,
+  `kode_pos` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`npm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
